b14b67e7d51b63e3fa3d1b22c17e27349f2db8ee
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-03-31 13:42:33 +0300

    filter and content components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProjectComponent } from './components/project/project.component';
+import { FilterItemComponent } from './components/filter-item/filter-item.component';
+import { FilterButtonComponent } from './components/filter-button/filter-button.component';
+import { ContentHeadingComponent } from './components/content-heading/content-heading.component';
+import { ContentItemComponent } from './components/content-item/content-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProjectComponent
+    ProjectComponent,
+    FilterItemComponent,
+    FilterButtonComponent,
+    ContentHeadingComponent,
+    ContentItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/content-heading/content-heading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-heading',
+  templateUrl: './content-heading.component.html',
+  styleUrls: ['./content-heading.component.scss']
+})
+export class ContentHeadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
